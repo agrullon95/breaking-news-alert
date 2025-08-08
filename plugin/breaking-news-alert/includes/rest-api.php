864dbb43afb4c7ec09610bc3791284d6fc3f3503
Anthony Grullon <agrullon95@gmail.com>
2025-08-08 19:12:18 -0400

    feat: Update alert retrieval to improve performance and clean up unused routes

--- plugin/breaking-news-alert/includes/rest-api.php
+++ plugin/breaking-news-alert/includes/rest-api.php
@@ -9,35 +9,25 @@ function bna_register_rest_routes(AlertQueueClient $sqsClient) {
         register_rest_route('bna/v1', '/alerts', [
             'methods' => 'GET',
             'callback' => function() use ($sqsClient) {
-                $messages = $sqsClient->getDecodedMessages(5);
-
-                error_log(print_r($messages, true));
+                $messages = $sqsClient->getDecodedMessages(1);
 
                 $alerts = [];
                 foreach ($messages as $msg) {
                     $alerts[] = [
-                        'id' => $msg['id'] ?? uniqid(),
-                        'message' => $msg['message'] ?? '',
-                        'time' => $msg['time'] ?? '',
-                        'receiptHandle' => $msg['receiptHandle'] ?? '',
+                        'id'             => $msg['id'] ?? uniqid(),
+                        'message'        => $msg['message'] ?? '',
+                        'time'           => $msg['time'] ?? '',
+                        'receiptHandle'  => $msg['receiptHandle'] ?? '',
+                        
+                        // 👇 Block-specific metadata
+                        'alertType'      => $msg['type'] ?? 'info',
+                        'isDismissible'  => $msg['dismissible'] ?? true,
                     ];
                 }
-
+                
                 return rest_ensure_response($alerts);
             },
             'permission_callback' => function() {
-                // return current_user_can('read');
-                return '__return_true';
-            },
-        ]);
-
-        register_rest_route('bna/v1', '/test', [
-            'methods' => 'GET',
-            'callback' => function() {
-                return rest_ensure_response(['success' => true, 'message' => 'Test route is working']);
-            },
-            'permission_callback' => function() {
-                // return current_user_can('read');
                 return '__return_true';
             },
         ]);
