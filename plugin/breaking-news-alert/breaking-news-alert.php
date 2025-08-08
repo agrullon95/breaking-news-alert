4ea4dbc6d66ac6e5fb23fd78acbf7719ef0fb305
Anthony Grullon <agrullon95@gmail.com>
2025-08-08 09:17:27 -0400

    feat: Refactor breaking news alert plugin to include admin page and REST API routes

--- plugin/breaking-news-alert/breaking-news-alert.php
+++ plugin/breaking-news-alert/breaking-news-alert.php
@@ -9,12 +9,14 @@
 defined( 'ABSPATH' ) || exit;
 
 require_once __DIR__ . '/vendor/autoload.php';
-// require_once __DIR__ . '/includes/SQSClient.php';
 require_once __DIR__ . '/includes/hooks.php';
+require_once __DIR__ . '/includes/admin-page.php';
+require_once __DIR__ . '/includes/rest-api.php';
 
-// use BNA\SQSClient;
 use BNA\AlertQueueClient;
 use function BNA\register_hooks;
+use function BNA\bna_test_sqs_client;
+use function BNA\bna_register_rest_routes;
 
 $region = defined('BNA_AWS_REGION') ? BNA_AWS_REGION : '';
 $key    = defined('BNA_AWS_ACCESS_KEY_ID') ? BNA_AWS_ACCESS_KEY_ID : '';
@@ -24,43 +26,13 @@ $queueUrl = defined('BNA_AWS_SQS_QUEUE_URL') ? BNA_AWS_SQS_QUEUE_URL : '';
 // Instantiate the SQS client with AWS credentials and queue URL
 $sqsClient = new AlertQueueClient($region, $key, $secret, $queueUrl);
 
-// Register WordPress hooks and pass in the SQS client instance
+bna_register_rest_routes($sqsClient);
+
 register_hooks($sqsClient);
-
-// function bna_test_sqs_client() {
-//     $region = defined('BNA_AWS_REGION') ? BNA_AWS_REGION : '';
-//     $key    = defined('BNA_AWS_ACCESS_KEY_ID') ? BNA_AWS_ACCESS_KEY_ID : '';
-//     $secret = defined('BNA_AWS_SECRET_ACCESS_KEY') ? BNA_AWS_SECRET_ACCESS_KEY : '';
-//     $queueUrl = defined('BNA_AWS_SQS_QUEUE_URL') ? BNA_AWS_SQS_QUEUE_URL : '';
-
-//     $sqsClient = new BNA\AlertQueueClient($region, $key, $secret, $queueUrl);
-
-//     // Send a test message
-//     $messageBody = json_encode(['test' => 'Hello from test!']);
-//     $sendResult = $sqsClient->sendMessage($messageBody);
-
-//     if ($sendResult) {
-//         error_log('SQS test message sent successfully.');
-//     } else {
-//         error_log('Failed to send SQS test message.');
-//         return;
-//     }
-
-//     // Receive messages
-//     $messages = $sqsClient->receiveMessages(1);
-//     foreach ($messages as $msg) {
-//         error_log('Received message: ' . $msg['Body']);
-
-//         // Delete message after processing
-//         $deleted = $sqsClient->deleteMessage($msg['ReceiptHandle']);
-//         error_log('Message deleted: ' . ($deleted ? 'yes' : 'no'));
-//     }
-// }
-
 
 function bna_register_block() {
     // if (defined('WP_DEBUG') && WP_DEBUG) {
-    //     bna_test_sqs_client();
+    //     bna_test_sqs_client($sqsClient);
     // }
     register_block_type( __DIR__ );
 }
